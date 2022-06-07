e97020cca8ebd807faf64e2b5470026b3c1d5eb4
Dani <dani.ramirez.madrid@gmail.com>
2022-06-07 08:16:39 +0200

    making flow bar

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -1,23 +1,49 @@
 import React from "react";
-
+import "./Flow.css";
 
 export default function Flow() {
-
-    return <div className="wrapper">
-        <div className="flow">
-            <div className="ball-1"> <img className="ball-1-svg" src="img/ellipse1.svg" alt="ellipse"></img></div>
-            <div className="line-1"></div>
-            <div className="ball-2"></div>
-            <div className="line-2"></div>
-            <div className="ball-3"></div>
-            <div className="line-3"></div>
-            <div className="ball-4"></div>
-
-
+  return (
+    <div className="wrapper">
+      <div className="flow">
+        <div className="ball-1 ball">
+          <img
+            className="ball-1-svg"
+            src="img/ellipse1.svg"
+            alt="ellipse"
+          ></img>
+          <img className="ball-1-svg" src="img/check.svg" alt="ellipse"></img>
         </div>
+        <div className="line-1">
+          <div className="innerLine"></div>
+        </div>
+        <div className="ball-2">
+          <img
+            className="ball-1-svg"
+            src="img/ellipse1.svg"
+            alt="ellipse"
+          ></img>
+        </div>
+        <div className="line-2">
+          <div className="innerLine"></div>
+        </div>
+        <div className="ball-3">
+          <img
+            className="ball-1-svg"
+            src="img/ellipse1.svg"
+            alt="ellipse"
+          ></img>
+        </div>
+        <div className="line-3">
+          <div className="innerLine"></div>
+        </div>
+        <div className="ball-4">
+          <img
+            className="ball-1-svg"
+            src="img/ellipse1.svg"
+            alt="ellipse"
+          ></img>
+        </div>
+      </div>
     </div>
+  );
 }
-
-
-
-
